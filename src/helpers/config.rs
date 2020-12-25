b277b5401d0f473ff87a72351d37c19977374164
leang27 <52003343+leang27@users.noreply.github.com>
2020-12-25 13:10:17 +0700

    change image path

--- src/helpers/config.rs
+++ src/helpers/config.rs
@@ -1,7 +1,8 @@
 pub fn ROOT_PATH() -> String {
     if cfg!(debug_assertions) {
         format!(
-            "{}/root/usr/share/system_settings",
+            // "{}/root/usr/share/system_settings",
+            "{}",
             env!("CARGO_MANIFEST_DIR")
         )
     } else {
