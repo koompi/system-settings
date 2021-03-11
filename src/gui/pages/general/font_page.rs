8905b6773978766619d32d42227cba868ce107ae
veasnama <maveasnatech@gmail.com>
2021-03-11 14:32:15 +0700

    Get system font list

--- src/gui/pages/general/font_page.rs
+++ src/gui/pages/general/font_page.rs
@@ -1,11 +1,11 @@
-use iced::{Text, text_input, Column, PickList, Slider,slider, pick_list, Element, Align, Length, Container,HorizontalAlignment, Row};
-use super::super::super::styles::{CustomSelect};
-use std::default;
+use super::super::super::styles::CustomSelect;
+use iced::{pick_list, slider, text_input, Align, Column, Container, Element, HorizontalAlignment, Length, PickList, Row, Slider, Text};
 use serde::{Deserialize, Serialize};
-use std::fs::File;
-use std::io::{prelude::*, Error, Result};
-use toml::{from_str, to_string_pretty};
+use std::default;
 use std::fs;
+use std::fs::File;
+use std::io::{prelude::*, Error};
+use toml::{from_str, to_string_pretty};
 const HOME: &'static str = env!("HOME");
 #[macro_export]
 macro_rules! select_display {
@@ -20,116 +20,98 @@ macro_rules! select_display {
     };
 }
 #[derive(Default, Debug, Clone)]
-pub struct FontStyle{
-    value : f32,
-    state:slider::State,
+pub struct FontStyle {
+    value: f32,
+    state: slider::State,
     selected_font: FontList,
-    font: pick_list::State<FontList>,
+    font: pick_list::State<OutputFont>,
     search: text_input::State,
-    
+    array_fonts: Vec<OutputFont>,
+    font_output: OutputFont,
 }
-#[derive(Debug,Clone, Copy)]
-pub enum FontMsg{
-    SliderChange(f32),  
-    FontChanged(FontList),
+#[derive(Debug, Clone)]
+pub enum FontMsg {
+    SliderChange(f32),
+    FontChanged(OutputFont),
     // FontInfo(FontInfo),
-
 }
-impl FontStyle{
-    pub fn new() -> Self{
-        Self{
-            state:slider::State::new(),
+pub fn read_font<P: AsRef<std::path::Path>>(path: P) -> Result<Vec<String>, io::Error> {
+    let mut entries = fs::read_dir(path)?.map(|res| res.map(|e| e.path())).collect::<Result<Vec<_>, io::Error>>()?;
+    entries.sort();
+    let mut list_font = vec![];
+    for path in &entries {
+        list_font.push(path.file_name().unwrap().to_str().unwrap().to_string());
+    }
+    Ok(list_font)
+}
+
+impl FontStyle {
+    pub fn new() -> Self {
+        let mut fonts: Vec<OutputFont> = vec![];
+        match read_font("/usr/share/fonts/TTF") {
+            Ok(list) => {
+                for f in list {
+                    fonts.push(OutputFont { font: f });
+                }
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+            }
+        }
+        let first_font = match fonts.get(0) {
+            Some(d) => d.font.clone(),
+            None => String::from("Font not found"),
+        };
+        Self {
+            state: slider::State::new(),
             selected_font: FontList::default(),
             font: pick_list::State::default(),
             search: text_input::State::new(),
+            array_fonts: fonts,
+            font_output: OutputFont { font: first_font },
             ..Self::default()
         }
-        
     }
-    pub fn update(&mut self, msg: FontMsg){
-        match msg{
-            FontMsg::SliderChange(x) => self.value = x,  
+    pub fn update(&mut self, msg: FontMsg) {
+        match msg {
+            FontMsg::SliderChange(x) => self.value = x,
             // FontMsg::FontInfo(info)=>{
             //     let font_info = to_string_pretty(&info).unwrap();
             //     writer("font.conf",&info ).unwrap();
-            // } 
+            // }
             FontMsg::FontChanged(font) => {
-                self.selected_font = font;
-                create_dir();    
-                let font_conf = to_string_pretty(&font).unwrap();
-                writer("font.conf",&font ).unwrap();
-            }    
-            
+                self.font_output = font;
+                // create_dir();
+                // let font_conf = to_string_pretty(&font).unwrap();
+                // writer("font.conf", &font).unwrap();
+            }
         }
-            
     }
-    pub fn view(&mut self) -> Element<FontMsg>{
-        let FontStyle { 
-            value, 
-            state, 
-            selected_font, 
-            font,
-            search,
-        } = self;
-        let font_size=Column::new()
+    pub fn view(&mut self) -> Element<FontMsg> {
+        let FontStyle { value, state, selected_font, font, search, .. } = self;
+        let font_size = Column::new()
             .padding(20)
             .align_items(Align::Start)
-            .push(
-                Text::new("Size").size(24),
-            )
+            .push(Text::new("Size").size(24))
             .spacing(10)
             .push(
-                Row::new()
-                    .align_items(Align::Center)
-                    .spacing(10)
-                    .push(     
-                        Slider::new(
-                            &mut self.state,
-                            0.0..=100.0,
-                            self.value,
-                            FontMsg::SliderChange,
-                        )
-                        .step(1.0),
-                        
-                    )
-                    .push(
-                        Row::new()
-                            .align_items(Align::Center)
-                            .push(Text::new(&self.value.to_string()).horizontal_alignment(HorizontalAlignment::Center).width(Length::Units(20)))
-                            .push(Text::new("%")),
-                    )
+                Row::new().align_items(Align::Center).spacing(10).push(Slider::new(&mut self.state, 0.0..=100.0, self.value, FontMsg::SliderChange).step(1.0)).push(
+                    Row::new()
+                        .align_items(Align::Center)
+                        .push(Text::new(&self.value.to_string()).horizontal_alignment(HorizontalAlignment::Center).width(Length::Units(20)))
+                        .push(Text::new("%")),
+                ),
             )
-            
             .spacing(20);
-        let font_choice=
-            Column::new()
-            .width(Length::Fill)
-            .padding(20)  
-            .push (Text::new("Standard Font:").size(24))
-            .spacing(20)
-            .push(
-                PickList::new(
-                    font,
-                    &FontList::ALL[..],
-                    Some(*selected_font),
-                    FontMsg::FontChanged,
-                )
+        let font_choice = Column::new().width(Length::Fill).padding(20).push(Text::new("Standard Font:").size(24)).spacing(20).push(
+            PickList::new(font, &self.array_fonts, Some(self.font_output.clone()), FontMsg::FontChanged)
                 .width(Length::Units(250))
                 // .height(Length::Units(20))
                 .text_size(18)
                 .style(CustomSelect::Default),
-            );
-        let whole_content = Column::new()
-            .align_items(Align::Center)
-            .push(font_size)
-            .push(font_choice)
-            .padding(20)
-            .spacing(10);
-        Container::new(whole_content)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .into()
-        
+        );
+        let whole_content = Column::new().align_items(Align::Center).push(font_size).push(font_choice).padding(20).spacing(10);
+        Container::new(whole_content).width(Length::Fill).height(Length::Fill).into()
     }
 }
 // #[derive(Debug, Default, Deserialize, Serialize)]
@@ -151,18 +133,14 @@ impl FontStyle{
 //         }
 //     }
 // }
-#[derive(Debug, Copy, Clone, Eq, PartialOrd, PartialEq,Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Eq, PartialOrd, PartialEq, Serialize, Deserialize)]
 pub enum FontList {
     Monospace,
     Arial,
-    Serif, 
+    Serif,
 }
 impl FontList {
-    const ALL: [FontList; 3] = [
-        FontList::Monospace,
-        FontList::Arial,
-        FontList::Serif,
-    ];
+    const ALL: [FontList; 3] = [FontList::Monospace, FontList::Arial, FontList::Serif];
 }
 select_display!(FontList,
     FontList::Monospace => "Monospace",
@@ -174,22 +152,32 @@ impl Default for FontList {
         FontList::Monospace
     }
 }
-pub fn reader(name: &str) -> Result<String> {
+pub fn reader(name: &str) -> Result<String, io::Error> {
     let path = std::path::Path::new(format!("{}/.config/koompi/font", HOME).as_str()).join(name);
 
-    std::fs::read_to_string(path)  
+    std::fs::read_to_string(path)
 }
-pub fn writer(name: &str, data: &FontList) -> Result<()> {
-    
+pub fn writer(name: &str, data: &FontList) -> Result<(), io::Error> {
     let path = std::path::Path::new(format!("{}/.config/koompi/font", HOME).as_str()).join(name);
-   
     let mut file = File::create(path).unwrap();
     match file.write_all(to_string_pretty(data).unwrap().as_bytes()) {
         Ok(_) => Ok(()),
         Err(e) => Err(e),
-        } 
+    }
 }
-pub fn create_dir() -> std::io::Result<()>{
-    fs::create_dir_all(format!("{}/.config/koompi/font",HOME))?;
+pub fn create_dir() -> std::io::Result<()> {
+    fs::create_dir_all(format!("{}/.config/koompi/font", HOME))?;
     Ok(())
+}
+use std::io;
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct OutputFont {
+    pub font: String,
+}
+use std::fmt;
+impl fmt::Display for OutputFont {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.font)
+    }
 }
--- src/gui/pages/general/general_page.rs
+++ src/gui/pages/general/general_page.rs
@@ -1,13 +1,13 @@
 // use super::super::super::styles::{CustomButton, CustomContainer, CustomRadio, CustomSelect};
-use iced::{Align, Column, Container, Element, Length,  Row, Rule, Scrollable, Text, scrollable};
+use crate::gui::styles::containers::ContainerStyle;
+use iced::{scrollable, Align, Column, Container, Element, Length, Row, Rule, Scrollable, Text};
 use iced_custom_widget as icw;
 use icw::components::Tab;
 use icw::components::{Icon, Icons};
-use crate::gui::styles::containers::ContainerStyle;
 // use serde::de::value;
+use super::font_page::{FontMsg, FontStyle};
+use super::icon_page::{IconMsg, IconStyle};
 use super::theme_page::{Theme, ThemeMsg};
-use super::font_page::{FontStyle, FontMsg};
-use super::icon_page::{IconStyle,IconMsg};
 
 // use crate::helpers::ROOT_PATH;
 
@@ -27,7 +27,6 @@ pub enum Choice {
     B,
     C,
     D,
-    
 }
 impl Default for Choice {
     fn default() -> Self {
@@ -45,23 +44,23 @@ pub enum GeneralMessage {
 impl General {
     pub fn new() -> Self {
         Self {
-        //    choice: Choice::new(),
-           ..Default::default()
+            //    choice: Choice::new(),
+            font_style: FontStyle::new(),
+            ..Default::default()
         }
-        
     }
     pub fn update(&mut self, msg: GeneralMessage) {
         match msg {
-            GeneralMessage::TabSelect(select)=>{
+            GeneralMessage::TabSelect(select) => {
                 self.choice = select;
             }
-            GeneralMessage::ThemeMsg(msg)=>{
+            GeneralMessage::ThemeMsg(msg) => {
                 self.theme.update(msg);
             }
-            GeneralMessage::FontMsg(msg)=>{
+            GeneralMessage::FontMsg(msg) => {
                 self.font_style.update(msg);
             }
-            GeneralMessage::IconMsg(msg)=>{
+            GeneralMessage::IconMsg(msg) => {
                 self.icon_style.update(msg);
             }
         }
@@ -79,100 +78,30 @@ impl General {
             .width(Length::Fill)
             .align_items(Align::Center)
             .spacing(10)
-            .push(
-                Tab::new(
-                    Choice::A,
-                    Some(self.choice),
-                    GeneralMessage::TabSelect,
-                    tab_content(Icons::Palette, "General"),
-                )
-                .width(Length::Fill)
-                .height(Length::Units(50)),
-            )
-            .push(
-                Tab::new(
-                    Choice::B,
-                    Some(self.choice),
-                    GeneralMessage::TabSelect,
-                    tab_content(Icons::Icons, "Icon"),
-                )
-                .width(Length::Fill)
-                .height(Length::Units(50)),
-            )
-            .push(
-                Tab::new(
-                    Choice::C,
-                    Some(self.choice),
-                    GeneralMessage::TabSelect,
-                    tab_content(Icons::MousePointer, "Cursor"),
-                )
-                .width(Length::Fill)
-                .height(Length::Units(50)),
-            )
-            .push(
-                Tab::new(
-                    Choice::D,
-                    Some(self.choice),
-                    GeneralMessage::TabSelect,
-                    tab_content(Icons::Font, "Font"),
-                )
-                .width(Length::Fill)
-                .height(Length::Units(50)),
-            );
-        let contnet = Column::new()
-            .height(Length::Fill)
-            .align_items(Align::Center)
-            .padding(20)
-            .push(match self.choice {
-                Choice::A => self.theme.view().map(move |msg| GeneralMessage::ThemeMsg(msg)),
-                Choice::B => self.icon_style.view().map(move |msg|GeneralMessage::IconMsg(msg)),
-                Choice::C => Text::new("C").into(),
-                Choice::D => self.font_style.view().map(move |msg| GeneralMessage::FontMsg(msg)),
-            });
-        let netsidebar_scroll = Scrollable::new(&mut self.scroll_content)
-            .push(row)
-            .padding(10)
-            .scrollbar_width(4)
-            .scroller_width(4);
+            .push(Tab::new(Choice::A, Some(self.choice), GeneralMessage::TabSelect, tab_content(Icons::Palette, "General")).width(Length::Fill).height(Length::Units(50)))
+            .push(Tab::new(Choice::B, Some(self.choice), GeneralMessage::TabSelect, tab_content(Icons::Icons, "Icon")).width(Length::Fill).height(Length::Units(50)))
+            .push(Tab::new(Choice::C, Some(self.choice), GeneralMessage::TabSelect, tab_content(Icons::MousePointer, "Cursor")).width(Length::Fill).height(Length::Units(50)))
+            .push(Tab::new(Choice::D, Some(self.choice), GeneralMessage::TabSelect, tab_content(Icons::Font, "Font")).width(Length::Fill).height(Length::Units(50)));
+        let contnet = Column::new().height(Length::Fill).align_items(Align::Center).padding(20).push(match self.choice {
+            Choice::A => self.theme.view().map(move |msg| GeneralMessage::ThemeMsg(msg)),
+            Choice::B => self.icon_style.view().map(move |msg| GeneralMessage::IconMsg(msg)),
+            Choice::C => Text::new("C").into(),
+            Choice::D => self.font_style.view().map(move |msg| GeneralMessage::FontMsg(msg)),
+        });
+        let netsidebar_scroll = Scrollable::new(&mut self.scroll_content).push(row).padding(10).scrollbar_width(4).scroller_width(4);
         let whole_content: Element<_> = Row::new()
             .width(Length::Fill)
             .height(Length::Fill)
-            .push(
-                Container::new(netsidebar_scroll.height(Length::Fill))
-                    .style(ContainerStyle::White)
-                    .width(Length::FillPortion(4))
-                    .height(Length::Fill),
-            )
+            .push(Container::new(netsidebar_scroll.height(Length::Fill)).style(ContainerStyle::White).width(Length::FillPortion(4)).height(Length::Fill))
             .push(Rule::vertical(10))
             .push(
-                Container::new(contnet.height(Length::Fill))
-                    .width(Length::FillPortion(9))
-                    .height(Length::Fill)
-                    .style(ContainerStyle::White), // .padding(10),
+                Container::new(contnet.height(Length::Fill)).width(Length::FillPortion(9)).height(Length::Fill).style(ContainerStyle::White), // .padding(10),
             )
             .into();
-        let container = Container::new(whole_content)
-            .width(Length::Fill)
-            .center_x()
-            .center_y();
-        Container::new(container)
-            .style(ContainerStyle::LightGray)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .padding(10)
-            .center_x()
-            .center_y()
-            .into()
+        let container = Container::new(whole_content).width(Length::Fill).center_x().center_y();
+        Container::new(container).style(ContainerStyle::LightGray).width(Length::Fill).height(Length::Fill).padding(10).center_x().center_y().into()
     }
 }
-    
 fn tab_content(icon: Icons, name: &str) -> Row<'static, GeneralMessage> {
-    Row::new()
-        .push(Icon::new(icon).size(24))
-        .push(Text::new(name).size(16))
-        .align_items(Align::Center)
-        .spacing(8)
+    Row::new().push(Icon::new(icon).size(24)).push(Text::new(name).size(16)).align_items(Align::Center).spacing(8)
 }
-
-
-
